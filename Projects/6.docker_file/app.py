5a0b186a385a514c557e2df2c9e3e30a438412e2
Nandhan-rao9 <22311a1903@ecm.sreenidhi.edu.in>
2025-07-26 18:45:58 +0530

    docker start

---
+++ Projects/6.docker_file/app.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    print("Addition:", add(a, b))
+    print("Subtraction:", subtract(a, b)) 
